6564716d684dac01364f956b4ddff3257e395097
f3anor <feanor@bernharsmacbook.home>
2013-10-25 17:30:55 +0200

    tmp menu gui fix;

--- unity/Assets/Scripts/Menus/MenuHighscore.cs
+++ unity/Assets/Scripts/Menus/MenuHighscore.cs
@@ -7,17 +7,26 @@ public class MenuHighscore : MenuMain {
 	
 	void OnGUI()
 	{
+		
+		//BG Image
+		GUI.Box (new Rect (0, 0, Screen.width, Screen.height), " ");
+		GUI.Label (new Rect (0, 0, Screen.width, Screen.height), tex_MenuBackground);
+		
+		//buttons
 		GUILayout.BeginHorizontal();
 		
-		if (GUILayout.Button("Start Game"))
+		if (GUILayout.Button(tex_ButtonStartGame))
 			Application.LoadLevel(GlobalNames.SCENE_ID_GAME);
-		if (GUILayout.Button("Options"))
+		if (GUILayout.Button(tex_ButtonOptions))
             Application.LoadLevel(GlobalNames.SCENE_ID_OPTIONS);
-		if (GUILayout.Button("Back to Main Menu"))
+		if (GUILayout.Button(tex_ButtonQuit))
             Application.LoadLevel(GlobalNames.SCENE_ID_MAINMENU);
 		
 		GUILayout.EndHorizontal();
 		
 		GUI.Label(new Rect(Screen.width/2, Screen.height/2, 100, 100), "Highscores");
+		
+		
+		//todo: draw highscores
 	}
 }
